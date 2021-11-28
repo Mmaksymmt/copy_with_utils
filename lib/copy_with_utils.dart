63b2fc87c3afa6a175d86adc7e0f19928bc84d20
Mmaksymmt <maxtel.2002@gmail.com>
2021-11-29 01:18:10 +0200

    Fix nullable, add tests

--- lib/copy_with_utils.dart
+++ lib/copy_with_utils.dart
@@ -1,6 +1,6 @@
 library copy_with_utils;
 
-typedef ValueCallback<T> = T Function()?;
+typedef ValueCallback<T> = T Function();
 
 T switcher<T>(ValueCallback<T>? valueCallback, T defaultValue) {
   return valueCallback == null ? defaultValue : valueCallback();
--- test/copy_with_utils_test.dart
+++ test/copy_with_utils_test.dart
@@ -1,1 +1,41 @@
-void main() {}
+import 'package:copy_with_utils/copy_with_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    "ValueCallback support null value",
+    () {
+      ValueCallback<int?>? callback;
+      expect(callback, equals(null));
+    },
+  );
+
+  test(
+    "ValueCallback support nullable parameter types",
+    () {
+      ValueCallback<int?>? callback;
+      callback = () => null;
+      expect(callback(), equals(null));
+    },
+  );
+
+  test(
+    "switcher() returns defaultValue if valueCallback is null",
+    () {
+      ValueCallback<int>? callback;
+      expect(callback, equals(null));
+      expect(switcher(callback, 42), equals(42));
+    },
+  );
+
+  test(
+    "switcher() returns valueCallback result if valueCallback is not null",
+    () {
+      ValueCallback<int>? callback;
+      expect(callback, null);
+      callback = () => 42;
+      expect(callback(), equals(42));
+      expect(switcher(callback, 41), equals(42));
+    },
+  );
+}
