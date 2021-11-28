31d6a678e7febc7666c9e067e993729581ba558f
Mmaksymmt <maxtel.2002@gmail.com>
2021-11-28 23:16:50 +0200

    Initial commit

---
+++ lib/copy_with_utils.dart
@@ -0,0 +1,7 @@
+library copy_with_utils;
+
+typedef ValueCallback<T> = T Function()?;
+
+T switcher<T>(ValueCallback<T>? valueCallback, T defaultValue) {
+  return valueCallback == null ? defaultValue : valueCallback();
+}
